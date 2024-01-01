cb292b460225ed3907f8545fcad7a7b042b84370
Fatih Mehmet Gurbuz <115665567+fatihnet75@users.noreply.github.com>
2024-01-01 22:20:51 +0300

    splash form mistake

--- RentaCar/SplashForm.cs
+++ RentaCar/SplashForm.cs
@@ -1,10 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -12,9 +6,46 @@ namespace RentaCar
 {
     public partial class SplashForm : Form
     {
+        private int remainingSeconds = 3; // Geri sayım için kalan süre
+
         public SplashForm()
         {
             InitializeComponent();
+        }
+
+        private async void SplashForm_Load(object sender, EventArgs e)
+        {
+            await PerformLoadingAsync();
+
+            Girir girirForm = new Girir();
+            girirForm.Show();
+
+            // 3 saniyelik geri sayımı başlat
+            await StartCountdown();
+        }
+
+        private async Task PerformLoadingAsync()
+        {
+            // Simüle edilmiş bir yükleme süresi
+            await Task.Delay(300); // 0.3 saniye
+        }
+
+        private async Task StartCountdown()
+        {
+            while (remainingSeconds > 0)
+            {
+                label1.Text = $"Kalan Süre: {remainingSeconds} saniye";
+                await Task.Delay(1000); // 1 saniye beklet
+                remainingSeconds--;
+            }
+
+            // Geri sayım tamamlandıktan sonra SplashForm'u kapat
+            Close();
+        }
+
+        private void SplashForm_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            // Eğer bu olay tetiklenirse, diğer formu açabilir veya başka işlemler yapabilirsiniz.
         }
     }
 }
